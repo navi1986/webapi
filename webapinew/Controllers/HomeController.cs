9e4c92926b37b191d60404c67f33ed4a83bc1f1f
gurnaiv singh boparai <navi@gurnaivs-iMac.local>
2018-11-10 15:11:03 +1000

    adding unit test and webapi

---
+++ webapinew/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webapinew.Models;
+using webapinew.Controllers;
+
+
+namespace webapinew.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    public IActionResult obj()
+    {
+      ViewData["Message"] = "Your api page.";
+      var val = new ValuesController();
+      var obj = val.Get();
+      return View(obj);
+    }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
