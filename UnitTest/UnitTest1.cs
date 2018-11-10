a9fb496c0a37d095d5642312bc3078046ab8d841
gurnaiv singh boparai <navi@gurnaivs-iMac.local>
2018-11-10 16:11:11 +1000

    adding unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,13 +1,19 @@
 using System;
 using Xunit;
-
+using webapinew.Controllers;
 namespace UnitTest
 {
     public class UnitTest1
     {
+    
+
         [Fact]
         public void Test1()
         {
+      var obj = new HomeController();
+      var res = new ValuesController();
+      res.Get();
+      Assert.Contains("navi", "navi");
 
         }
     }
