29412e9997c3584657ecb22e58f60d12db79acbc
navi1986 <nv.takhar@gmail.com>
2018-11-10 14:06:21 +1000

    Initial check-in of module webapinew

---
+++ Models/obj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace webapinew.Models
+{
+  public class obj
+  {
+
+    public string RequestId { get; set; }
+    public decimal UserId { get; set; }
+    public string Nmae { get; set; }
+
+
+  }
+}
